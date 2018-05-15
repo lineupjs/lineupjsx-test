eb50c49c4e416f3330747302074fe4b8fd9f7a1a
Samuel Gratzl <samuel_gratzl@gmx.at>
2018-05-15 21:30:42 +0200

    add simple lineup example

--- src/App.js
+++ src/App.js
@@ -1,18 +1,25 @@
 import React, { Component } from 'react';
+import {LineUp} from 'lineupjsx';
+import 'lineupjsx/build/LineUpJSx.css';
 import logo from './logo.svg';
 import './App.css';
+
+const arr = [];
+const cats = ['c1', 'c2', 'c3'];
+for (let i = 0; i < 50; ++i) {
+  arr.push({
+      a: Math.random() * 10,
+      d: 'Row ' + i,
+      cat: cats[Math.floor(Math.random() * 3)],
+     cat2: cats[Math.floor(Math.random() * 3)]
+   })
+ }
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <LineUp data={arr} />
       </div>
     );
   }
